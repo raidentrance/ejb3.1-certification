ee41fc1f3975341f09bfb721984f5c8d627e4df5
alejandro-neology <aagapito@neology-rdif.com>
2016-01-05 12:11:01 -0600

    remove local bean annotation

--- session-beans/src/main/java/com/raidentrance/session/beans/slsb/MathHelperBean.java
+++ session-beans/src/main/java/com/raidentrance/session/beans/slsb/MathHelperBean.java
@@ -1,17 +1,27 @@
 package com.raidentrance.session.beans.slsb;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
-import javax.ejb.LocalBean;
 
 /**
  *
  * @author alejandrobautista
  */
 @Stateless
-@LocalBean
 public class MathHelperBean {
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Initializing MathHelperBean");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroying MathHelperBean");
+    }
+
     public double areaCalulator(double radius) {
-        return Math.PI*Math.pow(radius, 2);
+        return Math.PI * Math.pow(radius, 2);
     }
 }
